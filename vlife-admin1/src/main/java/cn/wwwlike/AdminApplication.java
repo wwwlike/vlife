9ea2d336b2b8941faf301ee2cea46ded5b727d47
xiaoxin <249168991@qq.com>
2022-10-16 22:29:43 +0800

    提交部分

---
+++ vlife-admin1/src/main/java/cn/wwwlike/AdminApplication.java
@@ -0,0 +1,23 @@
+package cn.wwwlike;
+
+import cn.wwwlike.web.annotation.EnableRespWrap;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiaoyu
+ * @date 2022/6/17
+ */
+//打开出参数据包装
+@EnableRespWrap
+@SpringBootApplication(scanBasePackages ={
+        "cn.wwwlike.auth",
+        "cn.wwwlike.oa",
+        "cn.wwwlike.sys",
+        "cn.wwwlike.vlife.ts",
+        "cn.wwwlike.form"})
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class);
+    }
+}
