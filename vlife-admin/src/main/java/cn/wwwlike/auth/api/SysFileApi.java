e6d0f6883e4a9ff2312faa34c4744c5b30eca845
xiaoxin <249168991@qq.com>
2023-01-29 23:15:08 +0800

    修订： 上线问题修订

--- vlife-admin/src/main/java/cn/wwwlike/auth/api/SysFileApi.java
+++ vlife-admin/src/main/java/cn/wwwlike/auth/api/SysFileApi.java
@@ -10,12 +10,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
-import java.io.*;
-import java.net.URLEncoder;
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,6 +29,7 @@ public class SysFileApi extends VLifeApi<SysFile, SysFileService> {
 
     @Value("${file.image.path}")
     public String imgPath;
+
     /**
      * wangEditor上传图片接收,不存数据库
      */
@@ -38,7 +38,7 @@ public class SysFileApi extends VLifeApi<SysFile, SysFileService> {
         Map map1 = new HashMap();
         Map map2 = new HashMap();
         String url = service.uploadImg(file);
-        map2.put("url", "http://localhost:8288/oa/sysFile/image/"+url);
+        map2.put("url", "http://localhost:8288/oa/sysFile/image/" + url);
         map1.put("errno", 0);
         map1.put("data", map2);
         return NativeResult.success(map1);
@@ -48,39 +48,13 @@ public class SysFileApi extends VLifeApi<SysFile, SysFileService> {
     //通过produces 告知浏览器我要返回的媒体类型
     @GetMapping(value = "/image/{id}", produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_GIF_VALUE, MediaType.IMAGE_PNG_VALUE})
     public BufferedImage image(@PathVariable String id) throws IOException {
-        SysFile file=service.findOne(id);
-        String fileName=file==null?id:file.getFileName();
-        return ImageIO.read(new FileInputStream(new File(imgPath+"//"+fileName)));
-    }
-
-    @GetMapping(value = "/test")
-      public String test(){
-            return "abcd";
-      }
-
-
-    @GetMapping(value = "/test1")
-    public Map test1(){
-        Map map=new HashMap();
-        map.put("title","呜呜我还");
-        map.put("total",111);
-        return map;
-    }
-
-
-    @GetMapping(value = "/test2")
-    public List<Map> test2(){
-        List<Map> m= new ArrayList<Map>();
-        Map map=new HashMap();
-        map.put("title","呜呜我还");
-        map.put("total",111);
-
-        Map map2=new HashMap();
-        map2.put("title","呜呜我还1");
-        map2.put("total",222);
-        m.add(map);
-        m.add(map2);
-        return m;
+        SysFile file = service.findOne(id);
+        String fileName = file == null ? id : file.getFileName();
+        if (!System.getProperty("os.name").toLowerCase().contains("win") &&
+                !imgPath.startsWith("/")) {
+            imgPath = "/" + imgPath;
+        }
+        return ImageIO.read(new FileInputStream(new File(imgPath + "/" + fileName)));
     }
 
     /**
@@ -93,12 +67,11 @@ public class SysFileApi extends VLifeApi<SysFile, SysFileService> {
         SysFile ff = new SysFile();
         ff.setName(file.getOriginalFilename());
         ff.setFileName(fileName);
-//        ff.setUrl(url);
         ff.setSize(file.getSize() + "kb");
         service.save(ff);
         return ff;
     }
-    
+
 
     @GetMapping("/details")
     public List<SysFile> detail(String ids[]) {
