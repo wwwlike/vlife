2058a26c1ed6775cd48193f302d4cdc7614a8ccf
dlc_home_pc <249168991@qq.com>
2023-11-24 16:39:39 +0800

    登录页美化

--- vlife-admin-react/src/pages/login/index.tsx
+++ vlife-admin-react/src/pages/login/index.tsx
@@ -1,143 +1,111 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { localHistoryLoginUserName, useAuth } from "@src/context/auth-context";
 import "./login.css";
-import { useForm, useUrlQueryParam } from "@src/hooks/useForm";
+import { useForm } from "@src/hooks/useForm";
 import { useNavigate } from "react-router-dom";
-import { Notification, Spin } from "@douyinfe/semi-ui";
-import RegExp from "@src/util/expression";
-import {
-  RegisterDto,
-  register as serverReg, //服务端注册
-} from "@src/api/SysUser";
-import { Result } from "@src/api/base";
-import { useDebounceEffect, useInterval } from "ahooks";
-import { Modal } from "@douyinfe/semi-ui";
-
+import { giteeUrl } from "@src/api/SysUser";
+import backgroundImage from "@src/assets/login_bg.jpg";
+import loginLeftImage from "@src/assets/login_left.png";
 const Index: React.FC = () => {
   const localUsername = window.localStorage.getItem(localHistoryLoginUserName);
-  const { user, login, error } = useAuth();
+  const { user, login, error, giteeLogin } = useAuth();
   const navigate = useNavigate();
   const { values, errors, setFieldValue } = useForm<{
     username: string;
     password: string;
   }>({ username: localUsername || "", password: "" }, null);
-  const [count, setCount] = useState(0);
+
   useEffect(() => {
     if (user && user.id) {
       navigate("/");
     }
   }, [user]);
 
-  //async 意义
   const handelSubmit = async () => {
     await login(values);
   };
 
-  interface registerFlag {
-    flag: boolean;
-    msg?: string; //校验说明
-  }
-
-  const [open, setOpen] = useState<boolean>(false);
-  const [registerFlag, setRegisterFlag] = useState<registerFlag>({
-    flag: true,
-  });
+  const gitLogin = () => {
+    giteeUrl().then((d) => {
+      if (d.data) {
+        window.location.href = d.data;
+      } else {
+        alert("服务端没有启用gitee快捷登录");
+      }
+    });
+  };
 
   return (
     <div
-      className=" bg-fixed bg-cover bg-center  w-full
-      flex items-center h-screen
-      "
       style={{
-        backgroundImage: `url(https://wwwlike.gitee.io/vlife-img/bj.jpg) `,
+        backgroundImage: `url(${backgroundImage})`,
       }}
+      className="bg-fixed relative bg-cover bg-center w-full justify-center flex items-center h-screen"
     >
-      <div className="main-container login-wrapper ">
-        <div>
-          <section className="flex justify-between">
-            <div
-              style={{
-                fontSize: "22px",
-                color: "#262626",
-                fontWeight: 500,
-                lineHeight: "32px",
-              }}
-            >
-              密码登录
-            </div>
-            <div className=" absolute top-2 right-2">
-              <img
-                src="https://wwwlike.gitee.io/vlife-img/wxgzh.jpg"
-                className=" w-36 "
-              />
-              关注获取账号密码
-            </div>
-          </section>
-          <section className="mt-5">
-            <div className="flex flex-col">
-              <p className="text-red-500 pt-2 text-center ">{error}</p>
-              <div className="mb-1 pt-2 rounded">
-                <input
-                  type="text"
-                  placeholder="账号"
-                  id="username"
-                  value={values.username || ""}
-                  onChange={(evt) =>
-                    setFieldValue("username", evt.target.value)
-                  }
-                  className=" h-12 text-xl  rounded w-full text-gray-700 focus:outline-none border-b border-gray-300 focus:border-blue-400 transition duration-500 px-3 pb-3"
-                />
-              </div>
-              <div className="mb-2 pt-3 rounded ">
-                <input
-                  type="password"
-                  id="password"
-                  placeholder="密码"
-                  value={values.password || ""}
-                  onChange={(evt) =>
-                    setFieldValue("password", evt.target.value)
-                  }
-                  className="h-12 text-xl  rounded w-full text-gray-700 focus:outline-none border-b border-gray-300 focus:border-blue-400 transition duration-500 px-3 pb-3"
-                />
-              </div>
-              <div className="mb-12 relative"></div>
-              <button
-                className=" bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 rounded-md shadow-lg hover:shadow-xl transition duration-200"
-                onClick={handelSubmit}
-              >
-                登 录
-              </button>
-            </div>
-          </section>
-        </div>
-
+      <div
+        style={{ width: "748px", height: "334px" }}
+        className="  flex justify-center items-center"
+      >
         <div
-          onMouseOver={() => {}}
-          onMouseOut={() => {}}
-          className="absolute  cursor-pointer text-center w-full py-4 h-16 bottom-0 left-0  bg-cover bg-center"
           style={{
-            backgroundImage:
-              "url(https://static001.geekbang.org/resource/image/da/a9/da2f9b538e487ec57fecc41a5c88c9a9.png)",
-            // color: "#fa8919",
-            cursor: "pointer",
+            width: "398px",
+            backgroundImage: `url(${loginLeftImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
           }}
+          className="h-full bg-white rounded-l-3xl shadow-xl shadow-left shadow-top shadow-bottom"
+        ></div>
+        <div
+          style={{ width: "358px", height: "334px" }}
+          className="relative  flex justify-center items-center bg-white rounded-r-3xl shadow-xl shadow-right shadow-top shadow-bottom"
         >
-          <a href="http://qm.qq.com/cgi-bin/qm/qr?k=786134846" target="_blank">
-            技术支持QQ群：786134846
-          </a>
+          {/* <div className=" absolute top-1 right-3  ">
+            <img
+              src="https://wwwlike.gitee.io/vlife-img/wxgzh.jpg"
+              className=" w-24 "
+            />
+            关注获取账号
+          </div>
+          <div className="gitee" onClick={gitLogin}>
+            <div className="switch-tip">Gitee登录</div>
+          </div> */}
+          <div
+            style={{ width: "258px" }}
+            className=" flex flex-col  justify-center items-center"
+          >
+            <div className="text-center font-bold tracking-wider text-blue-400 opacity-100 text-2xl">
+              vlife低代码
+            </div>
+            <p className="text-red-500 pt-2 text-center ">{error}</p>
+            <div className="mb-1 pt-2 rounded">
+              <input
+                type="text"
+                placeholder="请输入账号"
+                id="username"
+                value={values.username || ""}
+                onChange={(evt) => setFieldValue("username", evt.target.value)}
+                className=" h-10 text-xl  rounded w-full text-gray-700 focus:outline-none border-b border-gray-300 focus:border-blue-400 transition duration-500 px-3 pb-3"
+              />
+            </div>
+            <div className="mb-2 pt-3 rounded ">
+              <input
+                type="password"
+                id="password"
+                placeholder="请输入密码"
+                value={values.password || ""}
+                onChange={(evt) => setFieldValue("password", evt.target.value)}
+                className="h-10  text-xl  rounded w-full text-gray-700 focus:outline-none border-b border-gray-300 focus:border-blue-400 transition duration-500 px-3 pb-3"
+              />
+            </div>
+
+            <button
+              className=" mt-4 w-64 bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 rounded-md shadow-lg hover:shadow-xl transition duration-200"
+              onClick={handelSubmit}
+            >
+              登 录
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="max-w-lg mx-auto text-center mt-12 mb-6">
-        <p className="text-white"></p>
-      </div>
-
-      <div className="max-w-lg mx-auto flex justify-center text-white">
-        {/* <a href="#" className="hover:underline">联系我们</a>
-        <span className="mx-3">•</span> */}
-        {/* <a href="#" className="hover:underline">
-          鄂ICP备2022008507号
-        </a> */}
       </div>
     </div>
   );
