94c8e5dc5048b8d617f82a3f1f7bf75ce6dc6d20
dlc_home_pc <249168991@qq.com>
2024-04-10 00:11:15 +0800

    v1.0.4

--- vlife-admin-react/src/workflow-editor/hooks/useExport.ts
+++ vlife-admin-react/src/workflow-editor/hooks/useExport.ts
@@ -1,9 +1,9 @@
-import { message } from "antd";
+// import { message } from "antd";
 import { useCallback } from "react";
 import { saveFile } from "../utils/saveFile";
 import { useTranslate } from "../react-locales";
 import { useStartNode } from "./useStartNode";
-
+import { Notification } from "@douyinfe/semi-ui";
 export function useExport() {
   const t = useTranslate();
   const startNode = useStartNode();
@@ -12,7 +12,10 @@ export function useExport() {
     saveFile(`approvalflow`, JSON.stringify({ startNode }, null, 2)).then(
       (savedName) => {
         if (savedName) {
-          message.success(t("operateSuccess"))
+          Notification.success({
+            content:t("operateSuccess"),
+          });
+          // message.success(t("operateSuccess"))
         }
       }
     ).catch(err => {
