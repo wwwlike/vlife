5ac48acce7777880d75ca2cef623da3530ca83bf
dlc_home_pc <249168991@qq.com>
2024-04-09 23:25:54 +0800

    v1.0.4

--- vlife-admin-react/src/workflow-editor/nodes/CloseButton.tsx
+++ vlife-admin-react/src/workflow-editor/nodes/CloseButton.tsx
@@ -1,8 +1,8 @@
-import { useCallback } from "react"
-import { useEditorEngine } from "../hooks"
-import { CloseOutlined } from "@ant-design/icons"
-import { styled } from "styled-components"
-import { Button } from "antd"
+import { useCallback } from "react";
+import { useEditorEngine } from "../hooks";
+// import { CloseOutlined } from "@ant-design/icons"
+import { styled } from "styled-components";
+import { Button } from "@douyinfe/semi-ui";
 
 const CloseStyledButton = styled(Button)`
   position: absolute;
@@ -13,27 +13,23 @@ const CloseStyledButton = styled(Button)`
   display: flex;
   justify-content: center;
   align-items: center;
-`
-export const CloseButton = ((
-  props: {
-    nodeId?: string
-  }
-) => {
-  const { nodeId } = props
-  const store = useEditorEngine()
+`;
+export const CloseButton = (props: { nodeId?: string }) => {
+  const { nodeId } = props;
+  const store = useEditorEngine();
 
   const handleClose = useCallback(() => {
-    store?.removeNode(nodeId)
-  }, [nodeId, store])
+    store?.removeNode(nodeId);
+  }, [nodeId, store]);
 
   return (
     <CloseStyledButton
       className="close"
-      type="text"
+      // type="text"
       size="small"
-      shape="circle"
-      icon={<CloseOutlined style={{ color: "#fff", fontSize: 12 }} />}
+      // shape="circle"
+      icon={<i className=" text-xl icon-close" />}
       onClick={handleClose}
     />
-  )
-})
+  );
+};
