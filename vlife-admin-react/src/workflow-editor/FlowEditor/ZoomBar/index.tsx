3d4c6817d524f00725a350e4206fcd69a4f7eeed
dlc_home_pc <249168991@qq.com>
2024-04-09 23:50:35 +0800

    v1.0.4

--- vlife-admin-react/src/workflow-editor/FlowEditor/ZoomBar/index.tsx
+++ vlife-admin-react/src/workflow-editor/FlowEditor/ZoomBar/index.tsx
@@ -1,8 +1,9 @@
-import { MinusOutlined, PlusOutlined } from "@ant-design/icons"
-import { Button, Space } from "antd"
-import { memo } from "react"
-import { styled } from "styled-components"
-import { canvasColor } from "../../utils/canvasColor"
+// import { MinusOutlined, PlusOutlined } from "@ant-design/icons"
+// import { Button, Space } from "antd"
+import { Button, Space } from "@douyinfe/semi-ui";
+import { memo } from "react";
+import { styled } from "styled-components";
+import { canvasColor } from "../../utils/canvasColor";
 
 export const MiniFloatContainer = styled.div`
   display: flex;
@@ -13,48 +14,56 @@ export const MiniFloatContainer = styled.div`
   padding: 4px 8px;
   border-radius: 5px;
   top: 16px;
-  &.float{
-    box-shadow: 0 2px 8px 0 rgba(0, 0, 0, ${props => props.theme.mode === "dark" ? "0.5" : "0.15"});
-    transform: ${props => props.theme.mode === "dark" ? "" : "scale(1.05)"};
+  &.float {
+    box-shadow: 0 2px 8px 0
+      rgba(
+        0,
+        0,
+        0,
+        ${(props) => (props.theme.mode === "dark" ? "0.5" : "0.15")}
+      );
+    transform: ${(props) => (props.theme.mode === "dark" ? "" : "scale(1.05)")};
   }
   transition: all 0.3s;
-  &.workflow-editor-zoombar{
+  &.workflow-editor-zoombar {
     right: 32px;
   }
-  &.workflow-operation-bar{
+  &.workflow-operation-bar {
     left: 32px;
   }
-`
+`;
 
-export const ZoomBar = memo((
-  props: {
-    float?: boolean,
+export const ZoomBar = memo(
+  (props: {
+    float?: boolean;
     zoom: number;
-    onZoomIn: () => void,
-    onZoomOut: () => void
-  }
-) => {
-  const { float, zoom, onZoomIn, onZoomOut } = props
+    onZoomIn: () => void;
+    onZoomOut: () => void;
+  }) => {
+    const { float, zoom, onZoomIn, onZoomOut } = props;
 
-  return (
-    <MiniFloatContainer className={"workflow-editor-zoombar" + (float ? " float" : "")}>
-      <Space>
-        <Button
-          type={"text"}
-          size="small"
-          icon={<MinusOutlined />}
-          disabled={zoom <= 0.1}
-          onClick={onZoomOut}
-        />
-        {Math.round(zoom * 100)}%
-        <Button
-          type={"text"}
-          size="small"
-          icon={<PlusOutlined />}
-          disabled={zoom >= 3}
-          onClick={onZoomIn}
-        />
-      </Space>
-    </MiniFloatContainer>
-  )
-})
+    return (
+      <MiniFloatContainer
+        className={"workflow-editor-zoombar" + (float ? " float" : "")}
+      >
+        <Space>
+          <Button
+            // type={"text"}
+            size="small"
+            icon={<i className=" text-base icon-remove_circle_outline1" />}
+            disabled={zoom <= 0.1}
+            onClick={onZoomOut}
+          />
+          {Math.round(zoom * 100)}%
+          <Button
+            // type={"text"}
+            size="small"
+            icon={<i className=" text-base icon-add_circle_outline" />}
+            disabled={zoom >= 3}
+            onClick={onZoomIn}
+          />
+        </Space>
+      </MiniFloatContainer>
+    );
+  }
+);
