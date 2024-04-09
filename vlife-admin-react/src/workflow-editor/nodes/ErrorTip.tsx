57a4115fb0ec1eb11f9580e8da610767dd44d006
dlc_home_pc <249168991@qq.com>
2024-04-09 23:22:31 +0800

    v1.0.4

--- vlife-admin-react/src/workflow-editor/nodes/ErrorTip.tsx
+++ vlife-admin-react/src/workflow-editor/nodes/ErrorTip.tsx
@@ -1,32 +1,29 @@
-import { InfoCircleOutlined } from "@ant-design/icons"
-import { Tooltip } from "antd"
-import { memo } from "react"
-import { styled } from "styled-components"
-import { useError } from "../hooks/useError"
-
+import { Tooltip } from "@douyinfe/semi-ui";
+import { memo } from "react";
+import { useError } from "../hooks/useError";
+import { styled } from "styled-components";
+// import { InfoCircleOutlined } from "@ant-design/icons";
+// const ErrorIcon = styled(InfoCircleOutlined)`
+//   color: red;
+//   font-size: 24px;
+// `;
 const Schell = styled.div`
   position: absolute;
   z-index: 2;
   top: 0;
   right: -40px;
-`
-const ErrorIcon = styled(InfoCircleOutlined)`
-  color:red;
-  font-size: 24px;
-`
+`;
 
-export const ErrorTip = memo((props: {
-  nodeId: string
-}) => {
-  const { nodeId } = props
-  const errorMsg = useError(nodeId)
+export const ErrorTip = memo((props: { nodeId: string }) => {
+  const { nodeId } = props;
+  const errorMsg = useError(nodeId);
   return (
     <Schell>
-      {
-        errorMsg && <Tooltip title={errorMsg}>
-          <ErrorIcon />
+      {errorMsg && (
+        <Tooltip content={errorMsg}>
+          <i className=" text-red-500 text-xl  icon-error_outline" />
         </Tooltip>
-      }
+      )}
     </Schell>
-  )
-})
+  );
+});
